3395c880a63cfc67b981fd6674f1389ec80387c1
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-01-05 17:08:52 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test
+{
+    public int x,y;
+    public void show()
+    {
+        System.out.println("Values are : "+x+" "+y);
+    }
+}
+class use extends Test
+{
+    void setData()
+    {
+        x=10;
+        y=20;
+    }
+}
+class pract7
+{
+    public static void main(String[] args)
+    {
+        use u1 = new use();
+        u1.setData();
+        u1.show();
+    }
+}
