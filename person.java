6186c3f4f83e1706f0c050ad200069444de0b768
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-01-05 16:26:54 +0530

    Update person.java

--- person.java
+++ person.java
@@ -9,7 +9,7 @@ class practical_2
     public static void main(String[] args)
     {
         person pr1 = new person();//object creation
-        pr1.name= "Rishav Parasar";
+        pr1.name= "Jack Sparrow";
         pr1.age=19;
         pr1.salary=35000;
         System.out.println("Name of the person is "+pr1.name);
