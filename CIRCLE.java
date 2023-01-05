f269b9ddb3bff8084468615bce028a7844678ebd
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-01-05 16:16:04 +0530

    Add files via upload

---
+++ CIRCLE.java
@@ -0,0 +1,20 @@
+ public class CIRCLE
+{
+    int x=0,y=0;
+    float radius;
+    public void display()
+    {
+        System.out.println("coordinate of the centre is : ("+ x +","+ y+")");
+        System.out.println("Radius of the circle is : " + radius );
+    }
+}
+class input
+{
+    public static void main(String[] args) {
+        CIRCLE c1 = new CIRCLE();
+        c1.x= 5;
+        c1.y= 7;
+        c1.radius= 5.5f;
+        c1.display();
+    }
+}
