4cda2b3786f8b261f764f9498ee45ab3d99f4a0a
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-01-05 17:04:59 +0530

    Update Overload_student.java

--- Overload_student.java
+++ Overload_student.java
@@ -1,21 +1,21 @@
-public class Overload_student {
+class Main {
     int id;
     String name;
-    Overload_student()
-    {
-        System.out.println("This is no-argument constructor");
+    Main()
+    { 
+        // done 
     }
-    Overload_student(int i,String n)
+    Main(int i,String n)
     {
         id=i;
         name=n;
     }
 
     public static void main(String[] args) {
-        Overload_student s = new Overload_student();
+        Main s = new Main();
         System.out.println("\n No-argument constructor values :\n");
         System.out.println("Student id : "+s.id+ "\nStudent name : "+ s.name);
-        Overload_student s2 = new Overload_student(10,"Rishav");
+        Main s2 = new Main(10,"Jack");
         System.out.println("\n parametric constructor values :\n");
         System.out.println("Student id : "+s2.id+ "\nStudent name : "+ s2.name);
     }
