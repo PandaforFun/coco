dbd78505e4d3792f04db55ca143651753171bfc3
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-01-05 16:11:52 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class palindrome
+{
+    static String n,n1 ="";
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the string: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextLine();
+        int i = n.length();
+        for (int p = (i - 1); p >=0; --p) {
+            n1 = n1 + n.charAt(p);
+        }
+
+        if (n.toLowerCase().equals(n1.toLowerCase())) {
+            System.out.println(n + " is a Palindrome String");
+        }
+        else {
+            System.out.println(n + " is not a Palindrome String");
+        }
+    }
+}
